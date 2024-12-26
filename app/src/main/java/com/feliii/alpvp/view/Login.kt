4f81132e0fa4353ac87c8e5dd9ad9786517def9e
Feli <feliciajoshlyn@gmail.com>
2024-12-26 22:06:01 +0700

    logreg view

---
+++ app/src/main/java/com/feliii/alpvp/view/Login.kt
@@ -0,0 +1,15 @@
+package com.feliii.alpvp.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun login(){
+
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun loginPreview(){
+    login()
+}
---
+++ app/src/main/java/com/feliii/alpvp/view/Register.kt
@@ -0,0 +1,15 @@
+package com.feliii.alpvp.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun register(){
+
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun registerPreview(){
+    register()
+}
