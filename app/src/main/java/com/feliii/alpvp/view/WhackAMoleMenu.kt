5f1e75bc2391258607843bba77d7c95d3b10d854
Feli <feliciajoshlyn@gmail.com>
2024-12-26 22:00:12 +0700

    Create WhackAMoleMenu.kt

---
+++ app/src/main/java/com/feliii/alpvp/view/WhackAMoleMenu.kt
@@ -0,0 +1,15 @@
+package com.feliii.alpvp.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WhackAMoleMenu() {
+    // Your Whack-A-Mole game UI goes here
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun WhackAMoleMenuPreview() {
+    WhackAMoleMenu()
+}
