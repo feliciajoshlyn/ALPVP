06e42ef158cb784da19798097caeb91998bfab80
Marcellas87 <meonggarong86862@outlook.com>
2025-01-02 10:24:27 +0700

    Update Home.kt

--- app/src/main/java/com/feliii/alpvp/view/Home.kt
+++ app/src/main/java/com/feliii/alpvp/view/Home.kt
@@ -1,5 +1,51 @@
 package com.feliii.alpvp.view
 
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.feliii.alpvp.R
+import com.feliii.alpvp.enums.PagesEnum
+import com.feliii.alpvp.viewmodel.AuthenticationViewModel
+import com.feliii.alpvp.viewmodel.HomeViewModel
+import com.feliii.alpvp.viewmodel.WAMViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -85,7 +131,7 @@ fun mainMenu(
             text = "whack a mole menu"
         )
     }
-    
+
     Button(
         onClick = { homeViewModel.logoutUser(token, navController) }
     ) {
