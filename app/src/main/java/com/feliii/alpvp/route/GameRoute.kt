c20b2f16765bf9db31eb521d7a9d08844b677551
Marcellas87 <meonggarong86862@outlook.com>
2025-01-04 09:03:58 +0700

    Home Minigmes Buttons!

--- app/src/main/java/com/feliii/alpvp/route/GameRoute.kt
+++ app/src/main/java/com/feliii/alpvp/route/GameRoute.kt
@@ -2,18 +2,34 @@ package com.feliii.alpvp.route
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.feliii.alpvp.R
 import com.feliii.alpvp.enums.PagesEnum
 import com.feliii.alpvp.view.MoodCalendar
 import com.feliii.alpvp.view.TodayMood
@@ -67,17 +83,77 @@ fun RelaxGameApp(
         }
 
         composable(route = PagesEnum.Home.name) {
-            mainMenu(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(20.dp),
-                wamViewModel = wamViewModel,
-                navController = navController,
-                context = localContext,
-                token = token.value,
-                username = username.value,
-                homeViewModel = homeViewModel
-            )
+            Scaffold(
+                bottomBar = {
+                    BottomAppBar(
+                        modifier = Modifier.height(96.dp),
+                        containerColor = Color(0xFF8871CA) //Blue Marguerite
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            // User bottom navbar
+                            Image(
+                                painter = painterResource(R.drawable.user_personoutline),
+                                contentDescription = "user",
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .size(48.dp)
+                                    .clickable {
+                                        /* logic here... */
+                                    }
+                                    .padding(2.dp)
+                            )
+
+                            // Minigame bottom navbar
+                            Image(
+                                painter = painterResource(R.drawable.minigame_window),
+                                contentDescription = "Minigame",
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .background(Color(0xFF644E96))
+                                    .size(72.dp)
+                                    .clickable {
+                                        /* logic here... */
+                                    }
+                                    .padding(8.dp)
+                            )
+
+                            // Mood Calendar bottom navbar
+                            Image(
+                                painter = painterResource(R.drawable.calendar_month),
+                                contentDescription = "calendar",
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .size(48.dp)
+                                    .clickable {
+                                        navController.navigate(PagesEnum.Calendar.name) {
+                                            popUpTo(PagesEnum.Calendar.name) {
+                                                inclusive = true
+                                            }
+                                        }
+                                    }
+                                    .padding(2.dp)
+                            )
+                        }
+                    }
+                }
+            ) { innerpadding ->
+                mainMenu(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp)
+                        .padding(innerpadding),
+                    wamViewModel = wamViewModel,
+                    navController = navController,
+                    context = localContext,
+                    token = token.value,
+                    username = username.value,
+                    homeViewModel = homeViewModel
+                )
+            }
         }
         composable(route = PagesEnum.WhackAMoleMenu.name) {
             WhackAMoleMenu(
